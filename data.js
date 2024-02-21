e1cfa9b590ea3b052732b20d72efce52bf189baa
Logan Lin <ylincs@bjtu.edu.cn>
2024-02-22 00:26:11 +0800

    Add more cards.

---
+++ data.js
@@ -0,0 +1,81 @@
+const index = {
+    "title": [
+        { "text": "Overleaf", "color": "#408827" },
+        { "text": "Copilot", "color": "#ff7300" }
+    ],
+    "slogan": "Seamlessly incorporate ChatGPT to power and accelerate academic writing in Overleaf.",
+    "head_img": "headline.webp",
+    "buttons": [
+        {
+            "url": "https://chromewebstore.google.com/detail/overleaf-copilot/eoadabdpninlhkkbhngoddfjianhlghb",
+            "text": "Add to browser",
+            "icon": "browser-chrome",
+            "tooltip": "Compatible with Chrome, Edge, Opera, Arc and other Chromium-based Browsers",
+            "style": "primary"
+        },
+        {
+            "url": "./premium-license.html",
+            "text": "Premium License",
+            "icon": "stars",
+            "style": "outline-primary"
+        }
+    ],
+    "cards": [
+        {
+            "title": "OpenAI Proxy, hassle-free access to GPT-3.5 and GPT-4.",
+            "texts": [
+                "No need for acquiring an OpenAI API Key. No need to understand the complex technical details of OpenAI API.",
+                "Easy and straight-forward access to OpenAI models. Free-trial included!"
+            ],
+            "img": "proxy.webp"
+        },
+        {
+            "title": "Highly customizable interface.",
+            "texts": [
+                "Versatile interface positioning and sizing. Integrate into your workflow however you like.",
+                "Deeply intergration with Overleaf. You may mistaken it as an native tool."
+            ],
+            "img": "interface.webp"
+        },
+        {
+            "title": "Highly customizable models and prompts.",
+            "texts": [
+                "Choose from a variety of GPT models to meet your editing requirements.",
+                "Customize prompts to align with your specific preferences."
+            ],
+            "img": "models_prompts.webp"
+        },
+        {
+            "title": "Rewriter, polish selected contents with one click of a button.",
+            "texts": [
+                "No more search for paper polishing prompts. No more constantly jump between ChatGPT and Overleaf.",
+                "Built-in high-quality prompts and integration with Overleaf, optimize your academic workflow."
+            ],
+            "img": "rewriter.webp"
+        },
+        {
+            "title": "Translator, a better choice for translating academic papers.",
+            "texts": [
+                "Intelligently distinguish LaTeX statements and equations between normal texts.",
+                "Retain academic style of writing and terminologies better than traditional translators."
+            ],
+            "img": "translator.webp"
+        },
+        {
+            "title": "Checker, a more robust grammar and spelling checker.",
+            "texts": [
+                "AI-powered checker to ensure no writing oversights.",
+                "Beautiful and practical comparison interface, intuitively see grammar and spelling errors."
+            ],
+            "img": "checker.webp"
+        },
+        {
+            "title": "Completor, write the next sentence for you.",
+            "texts": [
+                "Context-aware completion that helps you when you are out of ideas.",
+                "Capable of both writing the next sentence, or completing the current sentence."
+            ],
+            "img": "completor.webp"
+        }
+    ]
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+index['title'].forEach(({ text, color }, i) => {
+    var span = $(`<span style="color:${color};">${text}</span>`);
+    if (i > 0) {
+        span.addClass('ms-2');
+    }
+    $('#title').append(span);
+})
+
+$('#slogan').text(index['slogan']);
+
+index['buttons'].forEach(({ url, text, icon, style, tooltip }, i) => {
+    var button = $(`
+        <button type="button" class="btn btn-${style} btn-lg"
+            onClick="javascript:window.open('${url}', '_blank');"
+            data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="${tooltip}">
+            <i class="bi bi-${icon} pe-2"></i><span>${text}</span>
+        </button>
+    `);
+    if (tooltip) {
+        new bootstrap.Tooltip(button);
+    }
+    $('#button-row').append(button);
+})
+
+$('#headline').html(`
+    <img src="index_asset/${index['head_img']}" width="100%">
+`)
+
+index['cards'].forEach(({ title, texts, img }, i) => {
+    var card_body = $('<div class="card-body">');
+    var card_title = $(`<h5 class="card-title">${title}</h5>`);
+    var card_text = $('<div class="card-text">');
+    texts.forEach((text) => {
+        card_text.append(`<p class="mb-0 mt-1 small text-secondary">${text}</p>`);
+    })
+    card_body.append(card_title).append(card_text);
+
+    var card = $('<div class="card shadow-sm">');
+    card.append(`<img src="index_asset/${img}" class="card-img-top img-fluid rounded-start" alt="...">`);
+    card.append(card_body);
+    $('#function-row').append($('<div class="col function-col">').append(card));
+})
+
+function masonry_reload_on_images(parent_dom, item_selector) {
+    var images = parent_dom.find('img');
+    images.each(() => {
+        if (this.complete) {
+            $(this).trigger('load');
+        }
+    })
+    images.on('load', () => {
+        parent_dom.masonry({
+            itemSelector: item_selector,
+            columnWidth: item_selector,
+            transitionDuration: 500,
+            percentPosition: true
+        }).masonry('reloadItems').masonry('layout');
+    })
+}
+
+masonry_reload_on_images($('#function-row'), '.function-col')
