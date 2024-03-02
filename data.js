fb51c2a46458b9b0782c7ea4abe08f92cefea621
Logan Lin <ylincs@bjtu.edu.cn>
2024-03-02 16:54:09 +0800

    Add function.

--- data.js
+++ data.js
@@ -26,7 +26,7 @@ const index = {
     ],
     "cards": [
         {
-            "title": "OpenAI Proxy: Effortless Access to GPT-3.5 and GPT-4.",
+            "title": "OpenAI Proxy: Effortless Access to GPT-3.5 and GPT-4",
             "texts": [
                 "Eliminate the need for an OpenAI API Key and bypass the complexity of OpenAI API technicalities.",
                 "Gain direct and uncomplicated access to OpenAI models, with a free trial included!"
@@ -35,7 +35,7 @@ const index = {
             "badge": ["stars", "Premium"]
         },
         {
-            "title": "Auto-completion: Crafting the Next Sentence for You.",
+            "title": "Completer: Crafting the Next Sentence for You",
             "texts": [
                 "Benefit from context-aware suggestions that assist when you're stuck.",
                 "Efficiently complete or extend your sentences with ease."
@@ -44,7 +44,7 @@ const index = {
             "badge": ["stars", "Premium"]
         },
         {
-            "title": "Rewriter: Enhance Your Content with a Single Click.",
+            "title": "Rewriter: Enhance Your Content with a Single Click",
             "texts": [
                 "Forget the hassle of searching for paper polishing prompts and switching between ChatGPT and Overleaf.",
                 "Enjoy built-in high-quality prompts and seamless Overleaf integration to streamline your academic work."
@@ -52,7 +52,7 @@ const index = {
             "img": "rewriter.webp"
         },
         {
-            "title": "Translator: The Superior Choice for Academic Papers.",
+            "title": "Translator: The Superior Choice for Academic Papers",
             "texts": [
                 "Smartly differentiates LaTeX elements and regular text, preserving the academic integrity.",
                 "Maintains the scholarly tone and specific terminologies more effectively than standard translation tools."
@@ -60,7 +60,7 @@ const index = {
             "img": "translator.webp"
         },
         {
-            "title": "Checker: An Advanced Grammar and Spelling Tool.",
+            "title": "Checker: An Advanced Grammar and Spelling Tool",
             "texts": [
                 "Leverage AI to detect and correct any writing errors, ensuring flawless output.",
                 "Experience a user-friendly comparison interface that highlights grammar and spelling mistakes clearly."
@@ -68,7 +68,7 @@ const index = {
             "img": "checker.webp"
         },
         {
-            "title": "Highly Customizable Interface.",
+            "title": "Deep Integration with Overleaf",
             "texts": [
                 "Adjust interface placement and size to fit your workflow with versatility.",
                 "Experience deep integration with Overleaf, making it feel like a native tool."
@@ -76,7 +76,14 @@ const index = {
             "img": "interface.webp"
         },
         {
-            "title": "Customizable Models and Prompts.",
+            "title": "Highly Customizable Interface",
+            "texts": [
+                "Alter the interface's theme to match your personal preference."
+            ],
+            "img": "theme.webp"
+        },
+        {
+            "title": "Customizable Models and Prompts",
             "texts": [
                 "Select from various ChatGPT models tailored to your editing needs.",
                 "Personalize prompts to suit your unique requirements."
@@ -120,7 +127,7 @@ const premium_license = {
             "badge": ["stars", "Premium"]
         },
         {
-            "title": "Auto-completion: Crafting the Next Sentence for You.",
+            "title": "Completer: Crafting the Next Sentence for You.",
             "texts": [
                 "Benefit from context-aware suggestions that assist when you're stuck.",
                 "Efficiently complete or extend your sentences with ease."
