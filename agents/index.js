5e0f5ffd0957e47108d5d6a06fe10cc3d11f258d
Zhenjie Wei <Kogler_S@outlook.com>
2024-03-04 18:00:50 +0800

    feat: 优化Agents页面的展示效果

--- agents/index.js
+++ agents/index.js
@@ -1,26 +1,45 @@
+const indexes = new Map();
+
 fetch("./featured/indexes.json")
   .then((response) => response.json())
   .then((data) => {
+    const container = document.querySelector(".container");
     data.forEach((item) => {
       const card = document.createElement("div");
+      const id = item.name.replace(/ /g, "-").toLowerCase();
+      indexes.set(id, item);
       card.className = "card";
       card.innerHTML = `
-                <div class="icon">${item.icon}</div>
-                <div class="title">${item.name}</div>
-                <div class="description">${item.desc}</div>
-                <div class="source">
-                  Version: ${item.version}
-                  <br>
-                  Author: ${item.author}
-                  <br>
-                  Contact: 
-                  <a href="http://wpa.qq.com/msgr?uin=${
-                    item.contact.split(": ")[1]
-                  }&menu=yes" target="_blank">
-                    ${item.contact}
-                  </a>
+                <div class="index">
+                    <div class="title">${item.name}</div>
+                    <div class="description">${item.desc}</div>
+                    <div class="source">
+                        Version: ${item.version}
+                        <br>
+                        Author: ${item.author}
+                        <br>
+                    </div>
+                    <div class="button-container">
+                        <button class="outlined-button" onclick="copyTemp('${id}')">Copy Template</button>
+                        <button class="tonal-button" onclick="copyIndex('${id}')">Copy Index</button>
+                    </div>
+                </div>
+                <div class="preview">
+                    <img src="./featured/${id}.gif" alt="${item.name}.gif" />
                 </div>
               `;
-      document.body.appendChild(card);
+      container.appendChild(card);
     });
   });
+
+async function copyTemp(id) {
+  const data = await (await fetch(`./featured/${id}.xml`)).text();
+  console.log(data);
+  navigator.clipboard.writeText(data);
+}
+
+function copyIndex(id) {
+  const data = indexes.get(id);
+  console.log(data);
+  navigator.clipboard.writeText(JSON.stringify(data, null, 2));
+}
